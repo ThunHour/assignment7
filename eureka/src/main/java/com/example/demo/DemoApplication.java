20fb2e4a3546468f68e72474affe85d00f8c16d6
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-01-02 22:46:52 +0700

    "edit file"

--- eureka/src/main/java/com/example/demo/DemoApplication.java
+++ eureka/src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 @EnableEurekaServer
 @SpringBootApplication
 
